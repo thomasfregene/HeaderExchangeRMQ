e5bccce93e7c3a964383ec847e29ee9b0d1dbb7d
thomasfregene <fregzythomas@gmail.com>
2023-12-16 22:43:00 +0100

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Consumer
+
+var factory = new ConnectionFactory { HostName= "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("headersexchange", type: ExchangeType.Headers);
+
+channel.QueueDeclare(queue: "letterbox2");
+var bindingArgument = new Dictionary<string, object>
+{
+    {"x-match", "all" },
+    {"name", "brian" },
+    {"age", "31" },
+};
+channel.QueueBind("letterbox2", "headersexchange", "", bindingArgument);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received new message: {message}");
+};
+
+channel.BasicConsume(queue: "letterbox2", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Consuming");
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Producer
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.ExchangeDeclare(exchange: "headersexchange", type: ExchangeType.Headers);
+
+var message = "This message will be sent with headers";
+var body = Encoding.UTF8.GetBytes(message);
+
+var properties = channel.CreateBasicProperties();
+properties.Headers = new Dictionary<string, object>
+{
+    {"name", "brian" }
+};
+
+channel.BasicPublish("headersexchange", "", properties, body);
+Console.WriteLine($"Sending message");
